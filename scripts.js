77b4bc0aa3a1db19843e7d79f64fc79843c9402d
Nazirah Hanis <nazirahanis006@gmail.com>
2025-12-30 13:08:43 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -177,34 +177,37 @@ It enhanced my understandings and gave me a clear version of what we usually lea
 <b>🕐 Time:</b> 8.45 am - 5.00 pm <br>
 <b>📍 Venue:</b> Faculty of Computing, N28 
 
-## 🔭 Overview of the Programme
+<h3>🔭 Overview of the Programme</h3>
 Program Pembangunan Pelajar SECP 2025 was organized specially for Data Engineering students. The programme aimed to help students, especially first-year students, understand that our course isn’t just about theory, it’s also very practical and technical.
 
+<br>
 🔑 The program was divided into four main parts : <br>
 
-<b>1. UTM Graduate Attributes</b> <br>
+<br><b>1. UTM Graduate Attributes</b> <br>
 🎤 Mr Baharudin Mastari, Senior Deputy Registrar Faculty of Computing <br> 
 He reminded us that being a graduate is more than just getting good grades or knowing technical stuff. Our manners, professionalism and attitude matter just as much. Be a good person is as important as being a competent student. <br>
-🗨️ "Knowledge must be high, manners also must be high" 
+🗨️ "Knowledge must be high, manners also must be high" <br>
 
-<b>2. CV Writing</b> <br>
+<br> <b>2. CV Writing</b> <br>
 🎤 Ms. Hazwani Ramli, the founder of Get Me Hired <br>
 She gave some tips on how to make CV stand out. She emphasized that we should start building experiences now through side projects, joining competitions like hackathons or, volunteering as event crew. She also said that "Rejection is redirection", meaning that instead of feeling discouraged, see we should see rejections as redirections to help you grow. She also reminded to start being active now, so we can have meaningful content to put on the CV later.
+<br>
 
-<b>3. Learn Beyond the Classroom</b> <br>
+<br><b>3. Learn Beyond the Classroom</b> <br>
 🎤 sharing by Year 4 seniors <br>
 This part was super inspiring because the seniors shared their journey from Year 1 until now. They talked about internships, projects and clubs. They said that we shouldn't just rely only on lecturers’ assignments. Instead, create your own projects and self learning. Companies value students who show initiative. They also advised us to join clubs, competitions or events because it helps build skills and connections. 
-
+<br>
+<br>
 There are some advice that have truly resonated with me: <br>
 🗨️ "Learning is not a race, but it's about connecting and growing." — Neo Zheng Weng <br> 
 🗨️ "Master one stack first before moving to others. Go deep into one area until you truly understand it." — Yong Wern Jie <br>
 🗨️ "Don't just watch tutorial. Start actively practice what you learn." — Camily Tang Jia Lei  <br>
 🗨️ "Be honest. It's okay not to know everything. Interviews value honesty and willingness to learn more thab pretending to know." — Ng Shu Yu <br>
 
-<b>4. Mentor-Mentee Sessions</b> <br>
+<br> <b>4. Mentor-Mentee Sessions</b> <br>
 In this session, mentors from Year 2 and 3 shared their experiences and study tips. They reminded us that Year 1 is the best time to start being active. Even if you can’t join competitions as a participant, joining as a crew is still valuable experience. 
 
-## 📖 Reflection 
+<h3>📖 Reflection</h3>
 To be a successful and well-rounded graduate, we cannot just focus on academics but also need to develop leadership and management skills, be proactive in self-learning and creating personal projects and also build good personality and professionalism. It reminded me how important it is to develop other skills beyond what we learn in class, such as communication and leadership. The programme also encouraged us to grab every opportunity and explore more about today’s world of technology, as computer science is a field that’s highly in demand yet very competitive.
 It was a really insightful session, and I learned a lot about the skills and mindset needed to prepare for the future. ` }
         ]
@@ -326,6 +329,7 @@ window.onload = function () {
         localStorage.removeItem("skipLanding");
     }
 };
+
 
 
 
