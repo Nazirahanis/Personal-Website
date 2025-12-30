119bdf059be05b7d7a0d8169964215c179c1e314
Nazirah Hanis <nazirahanis006@gmail.com>
2025-12-30 13:02:04 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -120,9 +120,9 @@ From this talk, I realised that ICT is not only to have the knowledges and the s
 </ul>
 
 <h3>🔭 Overview of The Talk</h3>
-🎤 **Speaker** : Ts. Hj. Abdul Alim bin Abdul Muttalib, the Head of Technology and Innovation at Serunai Commerce Sdn. Bhd.
+🎤 <b>Speaker</b>: Ts. Hj. Abdul Alim bin Abdul Muttalib, the Head of Technology and Innovation at Serunai Commerce Sdn. Bhd.
 
-<h4>🔑 **Key Insight**</h4>
+<h4>🔑Key Insight</h4>
 <ul>
 <li>Speaker Experience</li>
 <li>What is Project Management and System Development?</li>
@@ -130,6 +130,8 @@ From this talk, I realised that ICT is not only to have the knowledges and the s
 <li>Skills Required for Computer Science and Industry</li>
 </ul>
 
+<h3>📝 Task</h3>
+To create an academic writing based on the talk that not exceed 3 pages.
 
 <h3>📖 Reflection</h3>
 To succeed in computer science, strong fundamentals, experience and skills are crucial. Classroom learning and group projects help to develop technical skills. In addition, participating in extracurricular activities enhances soft skills such as communication. Lastly, I should improve knowledge by exploring new tools such as using AI to complete tasks more efficiently.
@@ -140,8 +142,71 @@ To succeed in computer science, strong fundamentals, experience and skills are c
 <li>More time for Q&A session to allow students ask and learn directly about the industry.</li>
 </ul>
 
-<h3>📝 Task</h3>
-To create an academic writing based on the talk that not exceed 3 pages.` },
+` },
+		    { type: "Lab", title: "PC Assembly Class", description: 
+				
+`
+<b>🕐 Date:</b> 30 October 2025 <br>
+</b>🗓️ Time:</b> 4:00pm - 5:00pm
+
+<h3>🔭 Overview</h3>
+PC Assembly class was a part of the course learning, Technology, Information and System (TIS) 
+In this session, students were split into a few groups before the briefing about how to assemble and disassemble CPU components. 
+The session was conducted by a few staffs. After the session, we also had a quiz to recap about what we learn during the assembly.
+
+<h4>🔑 Activities</h4>
+<ul>
+<li>Observing the staff’s demonstration of CPU assembly and disassembly.</li>
+<li>Handling and installing components safely and accurately.</li>
+<li>Reassembling the PC and ensuring it boots successfully.</li>
+<li>Completing a quiz of the understanding based on the earlier session.</li>
+</ul>
+
+<h3>📝 Reflection</h3>
+For me, it was a valuable hands-on experience and opportunity to see and handle the components physically, not just theoretically. 
+It enhanced my understandings and gave me a clear version of what we usually learn in lectures. 
+
+<h3>Suggestion of Improvement</h3>
+<ul>
+<li>Add a troubleshooting segment to give more real-world experience and strengthen the problem-solving skills.</li>
+</ul>
+`},
+		   { type: "Program", title: "Program Pembangunan Pelajar SECP 2025", description: 
+`
+<b>🗓️ Date:</b> 8 November 2025 <br>
+<b>🕐 Time:</b> 8.45 am - 5.00 pm <br>
+<b>📍 Venue:</b> Faculty of Computing, N28 
+
+## 🔭 Overview of the Programme
+Program Pembangunan Pelajar SECP 2025 was organized specially for Data Engineering students. The programme aimed to help students, especially first-year students, understand that our course isn’t just about theory, it’s also very practical and technical.
+
+🔑 The program was divided into four main parts : <br>
+
+<b>1. UTM Graduate Attributes</b> <br>
+🎤 Mr Baharudin Mastari, Senior Deputy Registrar Faculty of Computing <br> 
+He reminded us that being a graduate is more than just getting good grades or knowing technical stuff. Our manners, professionalism and attitude matter just as much. Be a good person is as important as being a competent student. <br>
+🗨️ "Knowledge must be high, manners also must be high" 
+
+<b>2. CV Writing</b> <br>
+🎤 Ms. Hazwani Ramli, the founder of Get Me Hired <br>
+She gave some tips on how to make CV stand out. She emphasized that we should start building experiences now through side projects, joining competitions like hackathons or, volunteering as event crew. She also said that "Rejection is redirection", meaning that instead of feeling discouraged, see we should see rejections as redirections to help you grow. She also reminded to start being active now, so we can have meaningful content to put on the CV later.
+
+<b>3. Learn Beyond the Classroom</b> <br>
+🎤 sharing by Year 4 seniors <br>
+This part was super inspiring because the seniors shared their journey from Year 1 until now. They talked about internships, projects and clubs. They said that we shouldn't just rely only on lecturers’ assignments. Instead, create your own projects and self learning. Companies value students who show initiative. They also advised us to join clubs, competitions or events because it helps build skills and connections. 
+
+There are some advice that have truly resonated with me: <br>
+🗨️ "Learning is not a race, but it's about connecting and growing." — Neo Zheng Weng <br> 
+🗨️ "Master one stack first before moving to others. Go deep into one area until you truly understand it." — Yong Wern Jie <br>
+🗨️ "Don't just watch tutorial. Start actively practice what you learn." — Camily Tang Jia Lei  <br>
+🗨️ "Be honest. It's okay not to know everything. Interviews value honesty and willingness to learn more thab pretending to know." — Ng Shu Yu <br>
+
+<b>4. Mentor-Mentee Sessions</b> <br>
+In this session, mentors from Year 2 and 3 shared their experiences and study tips. They reminded us that Year 1 is the best time to start being active. Even if you can’t join competitions as a participant, joining as a crew is still valuable experience. 
+
+## 📖 Reflection 
+To be a successful and well-rounded graduate, we cannot just focus on academics but also need to develop leadership and management skills, be proactive in self-learning and creating personal projects and also build good personality and professionalism. It reminded me how important it is to develop other skills beyond what we learn in class, such as communication and leadership. The programme also encouraged us to grab every opportunity and explore more about today’s world of technology, as computer science is a field that’s highly in demand yet very competitive.
+It was a really insightful session, and I learned a lot about the skills and mindset needed to prepare for the future. ` }
         ]
     },
 
@@ -261,6 +326,7 @@ window.onload = function () {
         localStorage.removeItem("skipLanding");
     }
 };
+
 
 
 
